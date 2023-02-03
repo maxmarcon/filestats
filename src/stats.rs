7343cb8b51cb9676c9966259b3bcae9d37208307
Max Marcon <maxmarcon@gmx.net>
2023-02-03 21:40:07 +0100

    implement histogram

--- src/stats.rs
+++ src/stats.rs
@@ -1,10 +1,11 @@
 #[cfg(test)]
 mod test;
 
+#[derive(PartialEq, Debug)]
 struct Bucket {
     count: u32,
-    size_sum: u64,
-    max_size: u64,
+    sum: u64,
+    ceiling: u64,
 }
 
 struct Histogram {
@@ -13,23 +14,47 @@ struct Histogram {
 
 impl Histogram {
     fn new(limits: &[u64]) -> Self {
-        let buckets = limits
+        let mut buckets: Vec<Bucket> = limits
             .iter()
             .map(|limit| Bucket {
                 count: 0,
-                size_sum: 0,
-                max_size: *limit,
+                sum: 0,
+                ceiling: *limit,
             })
             .collect();
+
+        buckets.push(Bucket {
+            count: 0,
+            sum: 0,
+            ceiling: u64::MAX,
+        });
 
         Histogram { buckets: buckets }
     }
 
-    fn add(&mut self, size: u64) {}
+    fn add(&mut self, sample: u64) {
+        for mut bucket in self.buckets.iter_mut() {
+            if sample <= bucket.ceiling {
+                bucket.count += 1;
+                bucket.sum += sample;
+                break;
+            }
+        }
+    }
 
-    fn count(&self) -> u32 {}
+    fn count(&self) -> u32 {
+        self.buckets.iter().map(|bucket| bucket.count).sum()
+    }
 
     fn sum(&self) -> u64 {
-        0
+        self.buckets.iter().map(|bucket| bucket.sum).sum()
+    }
+
+    fn buckets(&self) -> usize {
+        self.buckets.len()
+    }
+
+    fn get_bucket(&self, index: usize) -> Option<&Bucket> {
+        self.buckets.get(index)
     }
 }
--- src/stats/test.rs
+++ src/stats/test.rs
@@ -1,14 +1,52 @@
 use super::Histogram;
+use crate::stats::Bucket;
 
 #[test]
-fn can_add_bucket() {
+fn can_add_samples() {
     let mut hist = Histogram::new(&vec![10, 20, 30]);
+    assert_eq!(hist.buckets(), 4);
 
     hist.add(5);
+    hist.add(7);
     hist.add(20);
+    hist.add(15);
+    hist.add(21);
     hist.add(25);
     hist.add(40);
 
-    assert_eq!(hist.count(), 4);
-    assert_eq!(hist.sum(), 90);
+    assert_eq!(hist.count(), 7);
+    assert_eq!(hist.sum(), 133);
+
+    assert_eq!(
+        hist.get_bucket(0),
+        Some(&Bucket {
+            count: 2,
+            sum: 7 + 5,
+            ceiling: 10
+        })
+    );
+    assert_eq!(
+        hist.get_bucket(1),
+        Some(&Bucket {
+            count: 2,
+            sum: 35,
+            ceiling: 20
+        })
+    );
+    assert_eq!(
+        hist.get_bucket(2),
+        Some(&Bucket {
+            count: 2,
+            sum: 46,
+            ceiling: 30
+        })
+    );
+    assert_eq!(
+        hist.get_bucket(3),
+        Some(&Bucket {
+            count: 1,
+            sum: 40,
+            ceiling: u64::MAX
+        })
+    );
 }
